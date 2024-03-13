69f9c8299a71dd87ead3137fe31bcb4970e55520
Alex <alex95.taylor@gmail.com>
2024-03-13 19:18:49 -0400

    Wrap application in redux store

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import Router from './Router';
+import { store } from './store.js';
+import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router />
+    <Provider store={store}>
+      <Router />
+    </Provider>
   </React.StrictMode>,
 )
