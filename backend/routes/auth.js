a7715342a9c3a411fb4ea7bcae9db795baac6ffd
Alex <alex95.taylor@gmail.com>
2024-03-12 20:11:56 -0400

    Change route name to register for continuity

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,7 +4,7 @@ const {checkAuth} = require('../authMiddleware');
 
 const authController = require('../controllers/authController');
 
-router.post('/signup', authController.register_user);
+router.post('/register', authController.register_user);
 router.post('/login', authController.login_user);
 
 module.exports = router;
