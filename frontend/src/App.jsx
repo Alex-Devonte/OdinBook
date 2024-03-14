3429480ef684c79ec31cffc4f335da0d52d73706
Alex <alex95.taylor@gmail.com>
2024-03-14 16:12:54 -0400

    Implement initial home component logic
    -Show welcome message and logout button if user is logged in

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,36 @@
 import './App.css'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { Link, useNavigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { logout, reset } from './features/auth/authSlice.js'
 
 function App() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const {user} = useSelector((state) => state.auth);
+
+  const onLogout = () => {
+    dispatch(logout());
+    dispatch(reset());
+  }
+
   return (
     <>
-      <h1 className='text-9xl'>OdinBook</h1>  
+      <h1 className='text-9xl'>OdinBook</h1>
+      {user ?
+        (
+          <>
+            <p>Welcome! You are currently signed in</p>
+            <button onClick={onLogout}>Logout</button>  
+          </>
+        ) : 
+        (
+          <>
+            <p>You aren't logged in</p>
+          </>
+        )}
+
     </>
   )
 }
