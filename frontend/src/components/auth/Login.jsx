d5208bfbc9ad05a2d7e9d4346708a2e53b11b3c6
Alex <alex95.taylor@gmail.com>
2024-03-14 16:13:59 -0400

    Implement login functionality into component
    -Update register link url

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -1,8 +1,31 @@
-import { useState  } from 'react';
-import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux';
+import { login, reset } from '../../features/auth/authSlice';
+import { toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Login() {
     const [formData, setFormData] = useState({email: '', password: ''});
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    //Select specific state needed for component
+    const {user, isLoading, isError, isSuccess, message} = useSelector((state) => state.auth);
+
+    useEffect(() => {
+        if (isError) {
+            toast.error(message);
+        }
+
+        //Navigate to home if user logs in successfully or is already logged in
+        if (isSuccess || user) {
+            navigate('/');
+        }
+    }, [user, isError, isSuccess, message, navigate, dispatch]);
+
     
     const handleChange = (e) => {
         setFormData((prevState) => ({
@@ -12,14 +35,24 @@ function Login() {
     }
 
     const handleSubmit = (e) => {
+        e.preventDefault();
 
+        const userData = {email: formData.email, password: formData.password};
+        dispatch(login(userData));
+    }
+
+    if (isLoading) {
+        return <p>Loading...</p>
     }
 
     return (
         <section>
             <h1 className='text-7xl mb-10'>Log In</h1>
             <form onSubmit={handleSubmit} className='grid grid-cols-2 gap-2 w-3/5 mx-auto'>
-                <div className='hidden col-span-full text-left mb-5'>Please fix the errors in red: </div>
+            <div 
+                className='text-red-300 text-left'>
+                {message}
+            </div>
                 <div className="col-span-full">
                     <input
                         type='text'
@@ -29,10 +62,6 @@ function Login() {
                         onChange={handleChange}
                         className='border-2 border-black p-0.5 w-full'
                     />
-                    <div 
-                        className='hidden text-red-300 text-left'>
-                        Error
-                    </div>
                 </div>
 
                 <div className="col-span-full">
@@ -44,10 +73,6 @@ function Login() {
                         onChange={handleChange}
                         className='border-2 border-black p-0.5 w-full'
                     />
-                    <div 
-                        className='hidden text-red-300 text-left'>
-                        Error
-                    </div>
                 </div>
 
                 <button 
@@ -56,7 +81,7 @@ function Login() {
                 >
                     Log In
                 </button>
-                <Link to='/signup' className='justify-start text-left mt-5'>Don&#39;t have an account? Sign up here</Link>
+                <Link to='/register' className='justify-start text-left mt-5'>Don&#39;t have an account? Sign up here</Link>
             </form>
         </section>
     )   
