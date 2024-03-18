227230ddfced5aebfef6c76170bdd33739dd0737
Alex <alex95.taylor@gmail.com>
2024-03-18 19:41:27 -0400

    Create Post model

---
+++ backend/models/post.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+    author: { type: Schema.Types.ObjectId, ref: 'User', required: true }, //ID of user that made the post
+    content: { type: String, maxLength: 100, required: true },
+    likes: { type: [{ type: Schema.Types.ObjectId, ref: 'User' }], default: [] }, //Array of user IDs that liked the post
+    comments: { type: [{ type: Schema.Types.ObjectId, ref: 'Comment' }], default: [] }, //Array of user IDs that commented on the post
+    createdDate: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Post', PostSchema);
