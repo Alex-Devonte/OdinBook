6d5b2992f2644220013678f8b9fd2ae86fda2ee7
Alex <alex95.taylor@gmail.com>
2024-03-18 19:46:51 -0400

    Create Header component
    -Display the user's name and profile picture
    -Contain link to user's profile
    -Contain link back to the homepage
    -Contain logout button

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,30 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { logout, reset } from '../features/auth/authSlice.js'
+import { Link } from 'react-router-dom';
+
+function Header() {
+    const {user} = useSelector((state) => state.auth);
+    const dispatch = useDispatch();
+
+    const onLogout = () => {
+        dispatch(logout());
+        dispatch(reset());
+    }
+
+    return (
+        <div className='flex items-center justify-between p-3'>
+            <div className='flex p-3'>
+                <Link to='/' className='text-5xl font-bold'>OdinBook</Link>
+            </div>
+            <div className='flex gap-5 items-center p-3'>
+                <Link to='/profile' className='flex items-center hover:underline gap-3'>
+                    <img src={user.profilePicture} className='w-10 rounded-full'/>
+                    <p>Hello, {user.firstName}!</p>
+                </Link>
+                <button onClick={onLogout}>Logout</button>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
