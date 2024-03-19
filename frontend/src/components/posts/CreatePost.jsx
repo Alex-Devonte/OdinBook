23e4a9e9b49b970cd5275c9feef33d7f3832c525
Alex <alex95.taylor@gmail.com>
2024-03-18 20:36:53 -0400

    Implement Toast to display error messages

--- frontend/src/components/posts/CreatePost.jsx
+++ frontend/src/components/posts/CreatePost.jsx
@@ -1,11 +1,21 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { createPost } from '../../features/posts/postSlice.js';
+import { toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function CreatePost() {
 
     const [content, setContent] = useState();
     const dispatch = useDispatch();
+    const {fieldErrors} = useSelector((state) => state.posts);
+
+    useEffect(() => {
+        if (fieldErrors) {
+            toast.error(fieldErrors.content);
+        }
+    }, [fieldErrors]);
 
     const handleCreatePost = () => {
         dispatch(createPost({content}));
@@ -14,6 +24,7 @@ function CreatePost() {
 
     return (
         <div className="container w-1/2 mx-auto bg-slate-600">
+            <ToastContainer />
             <div className="flex flex-col ">
                 <textarea
                     name="content"
