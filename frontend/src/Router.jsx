a097a5be06c17c3901670cc8b515f087c9715328
Alex <alex95.taylor@gmail.com>
2024-03-18 20:15:43 -0400

    Add profile & edit profile routes

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -2,6 +2,8 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import Profile from "./components/user/Profile";
+import EditProfile from "./components/user/EditProfile";
 
 const Router = () => {
     const router = createBrowserRouter([
@@ -16,6 +18,14 @@ const Router = () => {
         {
             path: '/login',
             element: <Login />,
+        },
+        {
+            path:'/profile',
+            element: <Profile />,
+        },
+        { 
+            path: 'profile/edit', 
+            element: <EditProfile /> 
         }
     ]);
 
