8f350366fe7ff7d9bb6c89f15dcd8ca70324189e
Alex <alex95.taylor@gmail.com>
2024-03-20 21:25:02 -0400

    Implement slice for handling user operations

---
+++ frontend/src/features/user/userSlice.js
@@ -0,0 +1,45 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import userService from './userService';
+
+const initialState = {
+    users: [],
+    isError: false,
+    isSuccess: false,
+    isLoading: false,
+    message: ''
+}
+
+export const getUsers = createAsyncThunk('user/getUsers', async (thunkAPI) => {
+    try {
+        return await userService.getUsers();
+    } catch (error) {
+        console.log(error);
+        return thunkAPI.rejectWithValue(error);
+    }
+});
+
+export const userSlice = createSlice({
+    name: 'users',
+    initialState,
+    reducers: {
+        reset: (state) => initialState,
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(getUsers.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(getUsers.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.isSuccess = true;
+                state.users = (action.payload);
+            })
+            .addCase(getUsers.rejected, (state, action) => {
+                state.isLoading = false;
+                state.isError = true;
+                state.message = action.payload;
+            })
+    }
+});
+
+export default userSlice.reducer;
