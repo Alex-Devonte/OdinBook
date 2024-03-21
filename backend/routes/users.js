d0bcb2d4e7c71d68dbef52fff864799b5f826c99
Alex <alex95.taylor@gmail.com>
2024-03-21 16:56:45 -0400

    Add route for following users

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,6 +4,7 @@ const userController = require('../controllers/userController');
 const {checkAuth} = require('../authMiddleware');
 
 router.get('/', checkAuth, userController.get_users);
+router.post('/followUser', checkAuth, userController.follow_user);
 
 
 module.exports = router;
