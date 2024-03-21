8f11730cf14f74e97ff874412279adb6db972f5d
Alex <alex95.taylor@gmail.com>
2024-03-21 16:55:25 -0400

    Implement logic for adding user to follower array in db

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,3 +5,21 @@ exports.get_users = asyncHandler(async (req, res, next) => {
     const users = await User.find({}).select('firstName lastName profilePicture').limit(5).exec();
     return res.json(users);
 });
+
+exports.follow_user = asyncHandler(async (req, res, next) => {
+    //IDs of currently logged in user and user that is being sent a follow request
+    const {currentUserID, requestUserID} = req.body;
+    const currentUser = await User.findById(currentUserID);
+    const requestUser = await User.findById(requestUserID);
+
+    //Add currentUser to follower list if they aren't already there
+    if (!requestUser.followers.some(follower => follower._id.toString() === currentUserID)) {
+        requestUser.followers.push(currentUserID);
+        const response = await requestUser.save();
+        console.log(response);
+        return res.json(response);
+    } else {
+        return res.json('You already follow this user.');
+    }
+
+});
