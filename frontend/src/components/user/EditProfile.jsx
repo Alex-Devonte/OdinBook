8e066bf9421001f9d453aaa2db7811d0dad45b5f
Alex <alex95.taylor@gmail.com>
2024-03-20 21:44:43 -0400

    Add initial component for Edit Profile

---
+++ frontend/src/components/user/EditProfile.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+function EditProfile() {
+
+     return (
+        <div>
+            <Link to='/profile'>Return to profile</Link>
+        </div>
+    )
+}
+
+export default EditProfile;
