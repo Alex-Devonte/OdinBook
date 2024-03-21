8697d0cc925664ff28d743ebeb1ee012c62ca32f
Alex <alex95.taylor@gmail.com>
2024-03-20 21:23:01 -0400

    Add userReducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './features/auth/authSlice';
 import postsReducer from './features/posts/postSlice';
+import userReducer from './features/user/userSlice';
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
         posts: postsReducer,
+        user: userReducer
     },
 })
