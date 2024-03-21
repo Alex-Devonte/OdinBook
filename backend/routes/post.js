1be94f7ab94d3358500291ab326559f65d680b6f
Alex <alex95.taylor@gmail.com>
2024-03-20 21:28:51 -0400

    Update route url and include controller

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const {checkAuth} = require('../authMiddleware');
 const postController = require('../controllers/postController');
 
+router.get('/', checkAuth, postController.get_posts);
+
 router.post('/create', checkAuth, postController.create_post);
 
 module.exports = router;
