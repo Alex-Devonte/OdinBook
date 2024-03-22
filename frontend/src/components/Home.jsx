0af07d9c50b53ce0b13b55a036e1cba8ba9341e8
Alex <alex95.taylor@gmail.com>
2024-03-22 18:00:11 -0400

    Add FollowUser component

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,10 +1,12 @@
 import CreatePost from './posts/CreatePost.jsx';
 import Feed from './posts/Feed.jsx';
 import DiscoverUsers from './user/DiscoverUsers.jsx';
+import FollowList from './user/FollowList.jsx';
 
 function Home() {
     return (
         <>
+                    <FollowList />
             <CreatePost />
             <Feed />
             <DiscoverUsers />
