6efaa53c5f26c0f8ab6cc7b10be6958b0bcb3edd
Alex <alex95.taylor@gmail.com>
2024-03-22 17:59:02 -0400

    Add post route for respond to request

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -5,6 +5,7 @@ const {checkAuth} = require('../authMiddleware');
 
 router.get('/', checkAuth, userController.get_users);
 router.post('/followUser', checkAuth, userController.follow_user);
+router.post('/respondToRequest', checkAuth, userController.respond_to_follow_request);
 
 
 module.exports = router;
