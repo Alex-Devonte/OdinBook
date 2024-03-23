6e76f1f241465e7ee97d63a10451e5f5c36ae8ed
Alex <alex95.taylor@gmail.com>
2024-03-23 18:31:35 -0400

    Add social reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,11 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './features/auth/authSlice';
 import postsReducer from './features/posts/postSlice';
 import userReducer from './features/user/userSlice';
+import socialReducer from './features/social/socialSlice';
+
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
         posts: postsReducer,
-        user: userReducer
+        user: userReducer,
+        social: socialReducer
     },
 })
