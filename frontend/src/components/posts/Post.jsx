a53eaeb9acd60047af1626b55d270ab7da96220e
Alex <alex95.taylor@gmail.com>
2024-03-24 19:15:11 -0400

    Add profile picture to post component

--- frontend/src/components/posts/Post.jsx
+++ frontend/src/components/posts/Post.jsx
@@ -4,6 +4,7 @@ function Post(props) {
     return (
         <div className='container'>
             <div className='border-2 border-b-black'>
+                <img src={post.author.profilePicture} className="w-10 rounded-full"/>
                 <p>Author: {post.author.firstName + ' ' + post.author.lastName}</p>
                 <p>Content: {post.content}</p>
                 <p>Comments: {post.comments.length}</p>
