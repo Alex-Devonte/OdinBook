e733bffe866951a1b107a37fb6a71ac5f2542cf0
Alex <alex95.taylor@gmail.com>
2024-03-25 20:24:54 -0400

    Implement function for handling post liking logic

--- frontend/src/components/posts/Post.jsx
+++ frontend/src/components/posts/Post.jsx
@@ -1,5 +1,15 @@
+import { useDispatch } from 'react-redux';
+import { likePost } from '../../features/posts/postSlice';
+
 function Post(props) {
     const {post} = props;
+    const dispatch = useDispatch();
+
+    const handleLikePost = () => {
+        dispatch(likePost(post._id));
+    };
+
+    console.log(post);
 
     return (
         <div className='container'>
@@ -9,6 +19,7 @@ function Post(props) {
                 <p>Content: {post.content}</p>
                 <p>Comments: {post.comments.length}</p>
                 <p>Likes: {post.likes.length}</p>
+                <button onClick={handleLikePost}>Click here to like</button>
             </div>
         </div>
     );
