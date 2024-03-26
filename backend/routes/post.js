b307c82ef062bc708771be6ba54a5e1b74c82262
Alex <alex95.taylor@gmail.com>
2024-03-25 20:21:11 -0400

    Add likePost route

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -6,5 +6,6 @@ const postController = require('../controllers/postController');
 router.get('/', checkAuth, postController.get_posts);
 
 router.post('/create', checkAuth, postController.create_post);
+router.post('/likePost', checkAuth, postController.like_post);
 
 module.exports = router;
