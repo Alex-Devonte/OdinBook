82247779cca0671e6150d279c21a52a3e751db8d
Alex <alex95.taylor@gmail.com>
2024-03-28 19:17:16 -0400

    Update post model schema
    -Add author field (Comment author)
    -Add text field (Comment text)

--- backend/models/post.js
+++ backend/models/post.js
@@ -5,7 +5,14 @@ const PostSchema = new Schema({
     author: { type: Schema.Types.ObjectId, ref: 'User', required: true }, //ID of user that made the post
     content: { type: String, maxLength: 240, required: true },
     likes: { type: [{ type: Schema.Types.ObjectId, ref: 'User' }], default: [] }, //Array of user IDs that liked the post
-    comments: { type: [{ type: Schema.Types.ObjectId, ref: 'Comment' }], default: [] }, //Array of user IDs that commented on the post
+    comments: { 
+        type: [{
+            author: { type: Schema.Types.ObjectId, ref: 'User', required: true }, //ID of user that made the comment
+            text: { type: String, maxLength: 120, required: true }, //Text content of the comment
+            createdDate: { type: Date, default: Date.now }
+        }],
+        default: [] 
+    },
     createdDate: { type: Date, default: Date.now }
 });
 
