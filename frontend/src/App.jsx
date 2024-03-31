8586015e9a947ab923cc9c7649d10e1b0508e08b
Alex <alex95.taylor@gmail.com>
2024-03-31 17:43:32 -0400

    Update component logic
    -Add state object to login redirect to send message
    -Add conditional for displaying content based on
     user logged in status

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,32 @@
 import './App.css'
 import { Link, useNavigate } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { logout, reset } from './features/auth/authSlice.js'  
 import { ToastContainer,toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Home from './components/Home.jsx';
 import Header from './components/Header.jsx';
+import { useEffect } from 'react';
 
 function App() {
   const navigate = useNavigate();
-  const dispatch = useDispatch();
   const {user} = useSelector((state) => state.auth);
 
-  const onLogout = () => {
-    dispatch(logout());
-    dispatch(reset());
-  }
+  useEffect(() => {
+    if (!user) {
+      navigate('/login', {state: {'errorMsg': 'You must be logged in to view that page'}});
+      window.history.replaceState({}, '');
+    }
+  }, [user, navigate]);
 
-  if (!user) {
-    
-    return <><p>You cant view this page since you arent logged in</p> <Link to='/login'>LOGOUT</Link></>
-  }
-  
   return (
     <>
-      <Header />
-      <Home />
+     {user && (
+      <>
+        <Header />
+        <Home />
+      </>
+    )}
     </>
   )
 }
