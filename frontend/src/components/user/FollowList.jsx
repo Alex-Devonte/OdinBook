cdf1775fcef2023ff08930d4157850ee8082f085
Alex <alex95.taylor@gmail.com>
2024-04-03 22:13:22 -0400

    Update import

--- frontend/src/components/user/FollowList.jsx
+++ frontend/src/components/user/FollowList.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { respondToFollowRequest } from '../../features/social/socialSlice';
+import { respondToFollowRequest } from '../../features/user/userSlice';
 
 function FollowList() {
     const dispatch = useDispatch();
