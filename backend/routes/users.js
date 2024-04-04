ee1b284571e078288a1dab40290394edce0fbf87
Alex <alex95.taylor@gmail.com>
2024-04-03 22:00:29 -0400

    Add social controller logic into user controller

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
-const socialController = require('../controllers/socialController');
 const {checkAuth} = require('../authMiddleware');
 
 router.get('/', checkAuth, userController.get_users);
-router.post('/followUser', checkAuth, socialController.follow_user);
-router.post('/respondToRequest', checkAuth, socialController.respond_to_follow_request);
+router.post('/followUser', checkAuth, userController.follow_user);
+router.post('/respondToRequest', checkAuth, userController.respond_to_follow_request);
 
 
 module.exports = router;
