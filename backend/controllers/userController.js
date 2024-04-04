05cc8317b7e1c57086ed8af81c15c0393abacf45
Alex <alex95.taylor@gmail.com>
2024-04-03 22:00:59 -0400

    Add social controller logic into user controller
    -Remove socialController since it is no longer needed

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,3 +5,50 @@ exports.get_users = asyncHandler(async (req, res, next) => {
     const users = await User.find({}).select('firstName lastName profilePicture').limit(10).exec();
     return res.json(users);
 });
+
+exports.follow_user = asyncHandler(async (req, res, next) => {
+    //IDs of currently logged in user and user that is being sent a follow request
+    const currentUserID = req.user._id;
+    const {requestUserID} = req.body;
+
+    const currentUser = await User.findById(currentUserID);
+    const requestUser = await User.findById(requestUserID);
+
+    //Add currentUser to follower list if they aren't already there
+    if (!requestUser.followers.some(follower => follower._id.toString() === currentUserID)) {
+        requestUser.followers.push(currentUserID);
+        const response = await requestUser.save();
+        console.log(response);
+        return res.json(response);
+    } else {
+        return res.json('You already follow this user.');
+    }
+
+});
+
+exports.respond_to_follow_request = asyncHandler(async (req, res, next) => {
+    const {userResponse, followerID} = req.body;
+    const respondingUserID = req.user._id;
+    
+    if (userResponse === 'accepted') {
+        //Find the follower with the specified ID and update the status
+        await User.findOneAndUpdate(
+            { _id: respondingUserID, 'followers._id': followerID },
+            { $set: { 'followers.$.status': userResponse } }
+        ).exec();
+
+        //After user accepts request, add them to the followers following array
+        await User.findByIdAndUpdate(followerID).updateOne({$push: {following:  respondingUserID}}).exec();
+    
+        res.json('Accepted');
+
+    } else if (userResponse === 'denied') {
+        //Remove user from followers array if request is denied
+        await User.findOneAndUpdate(
+            { _id: respondingUserID },
+            { $pull: { followers: { _id: (followerID) } } }
+        ).exec();
+
+        res.json('Denied request');
+    }
+});
