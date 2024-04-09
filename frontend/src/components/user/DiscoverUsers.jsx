96446d57436b8b3f3ea8796a47170076d881de96
Alex <alex95.taylor@gmail.com>
2024-04-08 21:17:13 -0400

    Implement check for sent requests

--- frontend/src/components/user/DiscoverUsers.jsx
+++ frontend/src/components/user/DiscoverUsers.jsx
@@ -6,6 +6,8 @@ import { useEffect } from 'react';
 function DiscoverUsers() {
     const dispatch = useDispatch();
     const {users, isLoading, isError, isSuccess, message} = useSelector((state) => state.user);
+    const currentUserID = useSelector((state) => state.auth.user._id);
+
 
     useEffect(() => {
         dispatch(getUsers());  
@@ -16,15 +18,27 @@ function DiscoverUsers() {
     };
 
     return (
-        <div className='border-2 border-black p-5 mx-5'>
-            <h1>Discover Users</h1>
+        <div className='bg-slate-300 rounded-lg p-5 mx-5'>
+            <h1 className='text-2xl mb-5'>Discover Users</h1>
             {users.length === 0 && <p>Currently no users to discover</p>}
             {users.map((user, i) => {
+                //Check if user already exists in the followers array
+                const isRequestSent = user.followers.some(follower => follower.user === currentUserID);
+
                 return (
-                    <div key={i} className="border border-black">
-                        <p>{user.firstName + ' ' + user.lastName}</p>
-                        <img className='w-10 rounded-full' src={user.profilePicture} />
-                        <button onClick={() => sendRequest(user._id)}>Send Friend Request</button>
+                    <div key={i} className='flex flex-col'>
+                        <div className='flex'>
+                            <img className='w-10 rounded-full' src={user.profilePicture} />
+                            <p>{user.firstName + ' ' + user.lastName}</p>
+                        </div>
+                        <div>
+                            {/* If request is already sent, show 'Request Sent' */}
+                            {isRequestSent ? (
+                                <button disabled>Request Sent</button>
+                            ) : (
+                                <button onClick={() => sendRequest(user._id)}>Send Friend Request</button>
+                            )}
+                        </div>
                     </div>
                 )
             })}
