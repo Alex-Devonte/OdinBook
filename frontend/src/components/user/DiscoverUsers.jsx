5b1b791647edec47c513372cdae2b9cc68c81495
Alex <alex95.taylor@gmail.com>
2024-04-09 20:51:49 -0400

    Update styles
    -Add color and spacing for clarity

--- frontend/src/components/user/DiscoverUsers.jsx
+++ frontend/src/components/user/DiscoverUsers.jsx
@@ -8,7 +8,6 @@ function DiscoverUsers() {
     const {users, isLoading, isError, isSuccess, message} = useSelector((state) => state.user);
     const currentUserID = useSelector((state) => state.auth.user._id);
 
-
     useEffect(() => {
         dispatch(getUsers());  
     }, [dispatch]);
@@ -24,19 +23,21 @@ function DiscoverUsers() {
             {users.map((user, i) => {
                 //Check if user already exists in the followers array
                 const isRequestSent = user.followers.some(follower => follower.user === currentUserID);
-
+                
                 return (
-                    <div key={i} className='flex flex-col'>
-                        <div className='flex'>
-                            <img className='w-10 rounded-full' src={user.profilePicture} />
-                            <p>{user.firstName + ' ' + user.lastName}</p>
+                    <div key={i} className='flex justify-between my-5'>
+                        <div className='flex items-center p-2'>
+                            <img className='flex justify-center w-20 rounded-full' src={user.profilePicture} />
                         </div>
-                        <div>
+                        <div className='flex items-center p-2'>
+                            <p className='text-lg'>{user.firstName + ' ' + user.lastName}</p>
+                        </div>
+                        <div className='flex items-center p-2'>
                             {/* If request is already sent, show 'Request Sent' */}
                             {isRequestSent ? (
-                                <button disabled>Request Sent</button>
+                                <button disabled className='bg-odin-gold text-white p-2 rounded-xl opacity-35 w-44 h-10'>Request Sent</button>
                             ) : (
-                                <button onClick={() => sendRequest(user._id)}>Send Friend Request</button>
+                                <button onClick={() => sendRequest(user._id)} className='bg-odin-gold text-white p-2 rounded-xl cursor-pointer w-44 h-10'>Send Friend Request</button>
                             )}
                         </div>
                     </div>
