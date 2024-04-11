8ff5bb06a7797c07a12b6097eaaf434ce667a0c5
Alex <alex95.taylor@gmail.com>
2024-04-11 15:19:36 -0400

    Update styles for different screen sizes

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -6,14 +6,14 @@ import FollowList from './user/FollowList.jsx';
 function Home() {
     return (
         <>
-         <div className="col-span-1">
+         <div className="col-span-full m-4 order-3 md:order-none md:col-span-1 md:m-0">
                 <DiscoverUsers />
             </div>
-            <div className="col-span-1">
+            <div className="col-span-full m-4 order-2  md:order-none md:col-span-1 md:m-0">
                 <CreatePost  />
                 <Feed />
             </div>
-            <div className="col-span-1">
+            <div className="col-span-full m-4 order-1 md:order-none md:col-span-1 md:m-0">
                 <FollowList />
             </div>
         </>
