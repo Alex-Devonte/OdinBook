addc552ffc2b570a4c2c481d5eb01ffc3d552ac4
Alex <alex95.taylor@gmail.com>
2024-04-11 17:32:18 -0400

    Add route for handling bio updates

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,6 +4,7 @@ const userController = require('../controllers/userController');
 const {checkAuth} = require('../authMiddleware');
 
 router.get('/', checkAuth, userController.get_users);
+router.put('/update/bio', checkAuth, userController.update_bio);
 router.post('/followUser', checkAuth, userController.send_follow_request);
 router.post('/respondToRequest', checkAuth, userController.respond_to_follow_request);
 
