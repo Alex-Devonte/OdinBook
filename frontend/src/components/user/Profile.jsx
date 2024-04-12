4e436d6ab1cb33591419e4dc3c945d8169500b69
Alex <alex95.taylor@gmail.com>
2024-04-12 10:18:35 -0400

    Implement logic for uploading profile picture

--- frontend/src/components/user/Profile.jsx
+++ frontend/src/components/user/Profile.jsx
@@ -2,7 +2,8 @@ import { useSelector, useDispatch } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import Header from '../Header'
 import { useState } from 'react';
-import { updateBio } from '../../features/user/userSlice';
+import { updateBio, uploadProfilePicture } from '../../features/user/userSlice';
+import { ToastContainer, toast } from 'react-toastify';
 
 function Profile() {
     const {user} = useSelector((state) => state.auth);
@@ -10,7 +11,9 @@ function Profile() {
 
     const [editMode, setEditMode] = useState(false);
     const [bio, setBio] = useState(user.bio);
-     
+    const [profilePicture, setProfilePicture] = useState(user.profilePicture);
+    const [fileData, setFileData] = useState({file: '', fileName:'', extension:''});
+
     //Send user to login page if they aren't logged in
     if (user === null) {
         return <Navigate to='/login' state={{type: 'auth', message: 'You must be logged in to access your profile'}}/>;
@@ -39,16 +42,62 @@ function Profile() {
         dispatch(updateBio({updatedBio: bio}));
         setEditMode(false);
     }
+
+    const handleFileChange = (e) => {
+        const file = e.target.files[0];
+
+        //Check if file is selected
+        if (file) {
+            //Extract file name and extension
+            const fileName = file.name;
+            const extension = fileName.split('.').pop().toLowerCase();
+
+            //Check if file is image with jpg or png extension
+            if (file.type.startsWith('image/') && extension === 'jpg' || extension === 'png') {
+                const reader = new FileReader();
+
+                reader.onload = () => {
+                    //Set profile picture to url of image after it has been read
+                    setProfilePicture(reader.result);
+                };
+        
+                //Read file contents and grab the URL
+                reader.readAsDataURL(file);
+                setFileData({file, fileName, extension});
+            }
+           
+        } else {
+            toast.error('Only file types of jpg or png are allowed');
+        }    
+    }
+
+    const handleUpload = () => {
+        //Check if uploaded picture is different from the original
+        if (fileData.file && profilePicture !== user.profilePicture) { 
+            const formData = new FormData();
+            const updatedFileData = {...fileData, imageURL: profilePicture};
+            setFileData(updatedFileData); 
+
+            formData.append('file', fileData.file);
+            formData.append('extension', fileData.extension);
+
+            dispatch(uploadProfilePicture(formData));
+        }
+    }
     
     return (
         <div className=' '>
             <Header />
+            <ToastContainer />
             <div className='flex flex-col items-center mt-10'>
                 <img
-                    src={user.profilePicture}
+                    src={profilePicture || user.profilePicture}
                     className='w-44 md:w-64 rounded-full mb-5'
                 />
-                <button className='bg-slate-700 text-white p-2 rounded-md mb-5 shadow-md'>Change Profile Picture</button>
+                <div className='flex flex-col'> 
+                    <input type='file' accept='image/jpeg, image/png' onChange={handleFileChange} className='bg-slate-700 text-white p-1 rounded-md mb-5 shadow-md'/>
+                    <button onClick={handleUpload} className='bg-slate-700 text-white p-2 rounded-md mb-5 shadow-md'>Change Profile Picture</button>
+                </div>
             </div>
             <div className='p-5 md:flex flex-col  md:w-1/4 md:mx-auto'>
                 <h2 className='text-2xl font-bold md:hidden'>Bio</h2>
