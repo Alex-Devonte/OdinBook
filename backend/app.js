f27db51afa771787b8cc257c54394e8d74d7509c
Alex <alex95.taylor@gmail.com>
2024-10-16 17:56:49 -0400

    Remove logic that serves frontend

--- backend/app.js
+++ backend/app.js
@@ -43,12 +43,6 @@ app.use('/api/auth', authRouter);
 app.use('/api/users', userRouter);
 app.use('/api/posts', postRouter);
 
-//Serve frontend
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/dist')));
-  app.get('*', (req, res) => 
-    res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html')));
-}
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
